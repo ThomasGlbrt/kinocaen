eb6338aae92894b1b792423656947bb742a0a226
ThomasGlbrt <guilbertthomas14@gmail.com>
2023-01-06 15:44:00 +0100

    Ajout formulaire de connexion et d'inscription

---
+++ src/Controller/UtilisateurController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UtilisateurController extends AbstractController
+{
+    public function index(UserPasswordHasherInterface $passwordHasher)
+    {
+        // ... e.g. get the user data from a registration form
+        $user = new User(...);
+        $plaintextPassword = ...;
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+
+        // ...
+    }
+}
