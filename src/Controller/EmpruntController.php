7d628cff62df346d647af2539720f54cb77afaf9
Louishrm03 <louisherman31@gmail.com>
2023-02-16 10:16:48 +0100

    MesEmprunts--empruntEnCours

--- src/Controller/EmpruntController.php
+++ src/Controller/EmpruntController.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,103 +11,237 @@ use App\Entity\Materiel;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Form\EmpruntType;
 use App\Form\EmpruntModifierType;
+use App\Form\EmpruntAdminType;
+
 class EmpruntController extends AbstractController
 {
-    #[Route('/emprunt', name: 'app_emprunt')]
+    /**
+     * Route principale pour la page d'emprunt.
+     *
+     * @Route("/emprunt", name="app_emprunt")
+     * 
+     * @return Response
+     */
     public function index(): Response
     {
         return $this->render('emprunt/index.html.twig', [
             'controller_name' => 'EmpruntController',
         ]);
     }
+
+
+    /**
+     * Afficher les détails d'un emprunt en particulier.
+     *
+     * @param ManagerRegistry $doctrine
+     * @param int $id
+     * 
+     * @return Response
+     */
     public function consulterEmprunt(ManagerRegistry $doctrine, int $id)
     {
+        // Récupération de l'emprunt correspondant à l'ID passé en paramètre
         $emprunt = $doctrine->getRepository(Emprunt::class)->find($id);
+
+        // Si aucun emprunt n'est trouvé, lancer une exception
         if (!$emprunt) {
             throw $this->createNotFoundException(
                 'Aucun emprunt trouvé avec le numéro '.$id
             );
         }
-        //return new Response('Emprunt : '.$emprunt->getNom());
+
+        // Retourner la vue associée à la consultation d'un emprunt
         return $this->render('emprunt/consulter.html.twig', [
             'emprunt' => $emprunt,
         ]);
     }
+
+
+    /**
+     * Afficher la liste de tous les emprunts.
+     *
+     * @param ManagerRegistry $doctrine
+     * 
+     * @return Response
+     */
     public function listerEmprunt(ManagerRegistry $doctrine)
     {
+        // Récupération du repository pour la classe Emprunt
         $repository = $doctrine->getRepository(Emprunt::class);
+
+        // Récupération de tous les objets Emprunt de la BDD
         $emprunt = $repository->findAll();
+
+        // Récupération de tous les objets Materiel de la BDD
         $materiel = $doctrine->getRepository(Materiel::class)->findAll();
+
+        // Renvoi de la vue avec les informations des emprunts et des matériels
         return $this->render('emprunt/lister.html.twig', [
             'pEmprunts' => $emprunt, 'pMateriels' => $materiel,
         ]);
     }
+
+
     public function ajouterEmprunt(ManagerRegistry $doctrine, Request $request, $id)
 {
+    // Instanciation d'un nouvel objet Emprunt
     $emprunt = new Emprunt();
+    // Récupération de l'utilisateur connecté
     $inscrit = $this->getUser()->getInscrit();
+    // Récupération du matériel à partir de son ID
     $materiel = $doctrine->getRepository(Materiel::class)->find($id);
+    // Association du matériel à l'emprunt
+    $emprunt->setMateriel($materiel);
+    // Association de l'utilisateur à l'emprunt
+    $emprunt->setInscrit($inscrit);
+    // Création du formulaire d'emprunt
     $form = $this->createForm(EmpruntType::class, $emprunt);
+    // Traitement des données du formulaire
     $form->handleRequest($request);
+    // Récupération des emprunts associés au matériel
+    $emprunts = $doctrine->getRepository(Emprunt::class)->findBy(['materiel' => $materiel]);
+    // Détermination de l'existence d'emprunts associés au matériel
+    $empruntExists = count($emprunts) > 0;
 
+    // Vérification si le matériel est déjà emprunté
+    if ($empruntExists) {
+        return $this->render('emprunt/ajouter.html.twig', [
+            'form' => $form->createView(),
+            'erreur' => 'Ce matériel est déjà emprunté.',
+            'pEmprunts' => $emprunts,
+            'empruntExists' => $empruntExists
+        ]);
+    }
+
+    // Si le formulaire a été soumis et est valide
     if ($form->isSubmitted() && $form->isValid()) {
-        $emprunt = $form->getData();
-        $emprunt->setInscrit($inscrit);
-        $emprunt->setMateriel($materiel);
-
+        // Persiste l'emprunt
         $entityManager = $doctrine->getManager();
         $entityManager->persist($emprunt);
         $entityManager->flush();
-
-        return $this->render('emprunt/consulter.html.twig', [
-            'emprunt' => $emprunt,
-        ]);
-    } else {
-        return $this->render('emprunt/ajouter.html.twig', [
-            'form' => $form->createView(),
-        ]);
+        return $this->redirectToRoute('empruntLister');
     }
+
+    return $this->render('emprunt/ajouter.html.twig', [
+        // Le formulaire est renvoyé avec la liste des emprunts.
+        'form' => $form->createView(),
+        'pEmprunts' => $emprunts,
+        'empruntExists' => $empruntExists
+    ]);
 }
+
 
     public function modifierEmprunt(ManagerRegistry $doctrine, $id, Request $request)
     {
-        //récupération du matériel dont l'id est passé en paramètre
-        $emprunt = $doctrine
-            ->getRepository(Emprunt::class)
-            ->find($id);
-     
+        // Récupère l'emprunt correspondant à l'id donné en paramètre
+        $emprunt = $doctrine->getRepository(Emprunt::class)->find($id);
+
+        // Si aucun emprunt n'est trouvé pour cet id
         if (!$emprunt) {
             throw $this->createNotFoundException('Aucun Emprunt trouvé avec le numéro '.$id);
         } else {
+            // Création du formulaire de modification d'emprunt à partir de l'objet EmpruntModifierType
             $form = $this->createForm(EmpruntModifierType::class, $emprunt);
+
+            // Traitement de la requête HTTP
             $form->handleRequest($request);
-     
+
+            // Si le formulaire a été soumis et est valide
             if ($form->isSubmitted() && $form->isValid()) {
+                // Récupération des données du formulaire
                 $emprunt = $form->getData();
+                // Persistence des données de l'emprunt dans la base de données
                 $entityManager = $doctrine->getManager();
                 $entityManager->persist($emprunt);
                 $entityManager->flush();
+
+                // Rendu de la vue pour consulter l'emprunt modifié
                 return $this->render('emprunt/consulter.html.twig', [
                     'emprunt' => $emprunt,
                 ]);
             } else {
+                // Sinon, rendu du formulaire de modification d'emprunt
                 return $this->render('emprunt/ajouter.html.twig', [
                     'form' => $form->createView(),
                 ]);
             }
         }
     }
+
+
     public function supprimerEmprunt(ManagerRegistry $doctrine, int $id, Request $request)
+    {
+        // Récupération de l'emprunt correspondant à l'id donné en paramètre
+        $emprunt = $doctrine->getRepository(Emprunt::class)->find($id);
+
+        // Si aucun emprunt n'est trouvé pour cet id
+        if (!$emprunt){
+            throw $this->createNotFoundException('Aucun emprunt trouvé avec cet id !');
+        }
+        else{
+            // Suppression de l'emprunt de la base de données
+            $entityManager = $doctrine->getManager();
+            $entityManager->remove($emprunt);
+            $entityManager->flush();
+        
+            // Redirection vers la liste des emprunts
+            return $this->redirectToRoute('empruntLister');
+        }
+    }
+
+
+    public function adminEmprunt(ManagerRegistry $doctrine, Request $request)
+    {
+        // Création d'un nouvel objet Emprunt
+        $emprunt = new Emprunt();
+        
+        // Création du formulaire lié à l'objet Emprunt
+        $form = $this->createForm(EmpruntAdminType::class, $emprunt);
+        
+        // Traitement des données du formulaire
+        $form->handleRequest($request);
+    
+        // Vérification si le formulaire a été soumis et si les données sont valides
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Récupération des données soumises par le formulaire
+            $emprunt = $form->getData();
+    
+            // Récupération du gestionnaire d'entités
+            $entityManager = $doctrine->getManager();
+    
+            // Persistence de l'objet Emprunt
+            $entityManager->persist($emprunt);
+    
+            // Enregistrement en base de données
+            $entityManager->flush();
+    
+            // Redirection vers la vue de consultation de l'emprunt
+            return $this->render('emprunt/consulter.html.twig', [
+                'emprunt' => $emprunt,
+            ]);
+        } else {
+            // Si le formulaire n'a pas été soumis ou les données sont invalides,
+            // redirection vers la vue de création de l'emprunt
+            return $this->render('emprunt/ajouterAdmin.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
+    }
+    
+    
+    public function consulterEmpruntsPerso(ManagerRegistry $doctrine, $id)
 {
-    $emprunt = $doctrine->getRepository(Emprunt::class)->find($id);
-    if (!$emprunt){
-        throw $this->createNotFoundException('Aucun emprunt trouvé avec cet id !');
-    }
-    else{
-        $entityManager = $doctrine->getManager();
-        $entityManager->remove($emprunt);
-        $entityManager->flush();
-        return $this->redirectToRoute('empruntLister');
-    }
+    // Récupération de l'utilisateur dont on veut consulter les emprunts
+    $user = $doctrine->getRepository(Inscrit::class)->find($id);
+
+    // Récupération de tous les emprunts associés à l'utilisateur
+    $emprunts = $doctrine->getRepository(Emprunt::class)->findBy(['inscrit' => $user]);
+
+    // Retourner la vue associée à la consultation des emprunts de l'utilisateur
+    return $this->render('emprunt/consulterPerso.html.twig', [
+        'pEmprunts' => $emprunts,
+    ]);
 }
+
+
 }
